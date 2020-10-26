d32ea9b721594df1abcb14b4793cf880be9719bd
Arhuan <aaronhuangwork@gmail.com>
2020-10-25 21:35:18 -0700

    note printing prototype

--- src/main/java/io/github/arhuan/app/model/Staff.java
+++ src/main/java/io/github/arhuan/app/model/Staff.java
@@ -1,6 +1,7 @@
 package io.github.arhuan.app.model;
 
 import io.github.arhuan.app.enums.Clef;
+import io.github.arhuan.app.enums.Note;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -17,17 +18,125 @@ public class Staff {
 
     public void printNotes() {
         // TODO: Add logic to print notes;
-        System.out.println("     ");
-        System.out.println("-----");
-        System.out.println("     ");
-        System.out.println("-----");
-        System.out.println("     ");
-        System.out.println("-----");
-        System.out.println("     ");
-        System.out.println("-----");
-        System.out.println("     ");
-        System.out.println("-----");
-        System.out.println("     ");
+        String line0 = "";
+        String line1 = "";
+        String line2 = "";
+        String line3 = "";
+        String line4 = "";
+        String line5 = "";
+        String line6 = "";
+        String line7 = "";
+        String line8 = "";
+        String line9 = "";
+        String line10 = "";
+
+        for (MusicalNote note : notes) {
+            switch (note.getNote()) {
+                case A:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("-----");
+                    line2 = line2.concat("     ");
+                    line3 = line3.concat("-----");
+                    line4 = line4.concat("     ");
+                    line5 = line5.concat("-----");
+                    line6 = line6.concat("  O  ");
+                    line7 = line7.concat("-----");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+                case B:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("-----");
+                    line2 = line2.concat("     ");
+                    line3 = line3.concat("-----");
+                    line4 = line4.concat("     ");
+                    line5 = line5.concat("--O--");
+                    line6 = line6.concat("     ");
+                    line7 = line7.concat("-----");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+                case C:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("-----");
+                    line2 = line2.concat("     ");
+                    line3 = line3.concat("-----");
+                    line4 = line4.concat("  O  ");
+                    line5 = line5.concat("-----");
+                    line6 = line6.concat("     ");
+                    line7 = line7.concat("-----");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+                case D:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("-----");
+                    line2 = line2.concat("     ");
+                    line3 = line3.concat("--O--");
+                    line4 = line4.concat("     ");
+                    line5 = line5.concat("-----");
+                    line6 = line6.concat("     ");
+                    line7 = line7.concat("-----");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+                case E:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("-----");
+                    line2 = line2.concat("  O  ");
+                    line3 = line3.concat("-----");
+                    line4 = line4.concat("     ");
+                    line5 = line5.concat("-----");
+                    line6 = line6.concat("     ");
+                    line7 = line7.concat("-----");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+                case F:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("--O--");
+                    line2 = line2.concat("     ");
+                    line3 = line3.concat("-----");
+                    line4 = line4.concat("     ");
+                    line5 = line5.concat("-----");
+                    line6 = line6.concat("     ");
+                    line7 = line7.concat("-----");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+                case G:
+                    line0 = line0.concat("     ");
+                    line1 = line1.concat("-----");
+                    line2 = line2.concat("     ");
+                    line3 = line3.concat("-----");
+                    line4 = line4.concat("     ");
+                    line5 = line5.concat("-----");
+                    line6 = line6.concat("     ");
+                    line7 = line7.concat("--O--");
+                    line8 = line8.concat("     ");
+                    line9 = line9.concat("-----");
+                    line10 = line10.concat("     ");
+                    break;
+            }
+        }
+
+        System.out.println(line0);
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+        System.out.println(line8);
+        System.out.println(line9);
+        System.out.println(line10);
     }
 
     public void addNote(MusicalNote note) {
