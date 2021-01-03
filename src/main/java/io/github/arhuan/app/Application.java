ca7f4b8f04219630057d2a86f66260d25d6b8c9b
Arhuan <aaronhuangwork@gmail.com>
2021-01-02 22:06:00 -0800

    fix CORs error (to revisit: make more restrictive)

--- src/main/java/io/github/arhuan/app/Application.java
+++ src/main/java/io/github/arhuan/app/Application.java
@@ -2,7 +2,10 @@ package io.github.arhuan.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaRepositories(basePackages = "io.github.arhuan.app.repository")
 @SpringBootApplication
@@ -10,5 +13,18 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            // TODO: Make this more restrictive later
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
