154d205ceb9277996148642fbc45259506c9f553
Arhuan <aaronhuangwork@gmail.com>
2021-01-05 20:52:54 -0800

    update CORS

--- src/main/java/io/github/arhuan/app/Application.java
+++ src/main/java/io/github/arhuan/app/Application.java
@@ -23,7 +23,7 @@ public class Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")
-                        .allowedOrigins("*");
+                        .allowedOrigins("http://localhost:3000");
             }
         };
     }
