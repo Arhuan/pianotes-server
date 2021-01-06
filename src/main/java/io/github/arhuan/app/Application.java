02cb0302597ff1be331b76c8fd1cc4c6b9291ef4
Arhuan <aaronhuangwork@gmail.com>
2021-01-05 20:53:44 -0800

    update CORS with heroku

--- src/main/java/io/github/arhuan/app/Application.java
+++ src/main/java/io/github/arhuan/app/Application.java
@@ -23,7 +23,7 @@ public class Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")
-                        .allowedOrigins("http://localhost:3000");
+                        .allowedOrigins("http://localhost:3000, https://pianotes.herokuapp.com");
             }
         };
     }
